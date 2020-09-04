ad75f203e29399c5b7c9018b3fcdc2b81cc81a90
dpandya <pandya_devarshi97@yahoo.com>
2020-09-03 23:50:26 -0300

    delete and clear feature for shopping-edit.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -11,8 +11,14 @@ export class ShoppingListService {
 
   ingredientsChanged = new Subject<Ingredient[]>();
 
+  startedEditing = new Subject<number>();
+
   getIngredients = () => {
     return this.ingredients.slice();
+  };
+
+  getIngredient = (index: number) => {
+    return this.ingredients[index];
   };
 
   addIngredient = (ingredient: Ingredient) => {
@@ -22,6 +28,16 @@ export class ShoppingListService {
 
   addIngredients = (ingredients: Ingredient[]) => {
     this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  };
+
+  updateIngredient = (index: number, newIngredient: Ingredient) => {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  };
+
+  deleteIngredient = (index: number) => {
+    this.ingredients.splice(index, 1);
     this.ingredientsChanged.next(this.ingredients.slice());
   };
 }
